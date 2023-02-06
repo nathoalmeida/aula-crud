e207209edf67b3b51ea48dc136be26637e96de85
nathoalmeida <nathalia.oalmeida@gmail.com>
2023-02-06 17:35:38 -0300

    funcao excluir usuario

--- listar-usuario.php
+++ listar-usuario.php
@@ -23,7 +23,7 @@
             print "<td>".$row->data_nascimento."</td>";
             print"<td>
                 <button onclick=\"location.href='?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
-                <button class='btn btn-danger'>Excluir</button>
+                <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=".$row->id."';}else{false;} \" class='btn btn-danger'>Excluir</button>
                 </td>";
             print"</tr>";
         }
--- salvar-usuario.php
+++ salvar-usuario.php
@@ -46,6 +46,18 @@
             break;
 
         case 'excluir':
+            
+            $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+
+            $res = $conn->query($sql);
+
+            if($res==true) {
+                print"<script>alert('Cadastro excluído com sucesso');</script>";
+                print"<script>location.href='?page=listar';</script>";
+            } else {
+                print"<script>alert('Não foi possível excluir');</script>";
+                print"<script>location.href='?page=listar';</script>";
+            }
 
             break;
     }
