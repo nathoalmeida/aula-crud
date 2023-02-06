e63f8b6435f405ae2c8414937d76a4873dc07d88
nathoalmeida <nathalia.oalmeida@gmail.com>
2023-02-06 09:32:38 -0300

    parte 1 aula CRUD

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Cadastro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=novo">Novo Usuário</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=listar">Listar Usuários</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+     <div class="col mt-5">
+
+<?php
+
+    include("config.php");
+    
+        switch(@$_REQUEST["page"]) {
+            case "novo":
+                include("novo-usuario.php");
+                break;
+            case "listar":
+                include("listar-usuario.php");
+                break;
+            default:
+                print "<h1>Bem-vindos</h1>";
+        }
+?>
+</div>
+    </div>
+        </div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ listar-usuario.php
@@ -0,0 +1,1 @@
+<h1>Listar usuários</h1>
